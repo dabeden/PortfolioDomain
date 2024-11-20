e70acb2597d4040539144e15502b73cef3d7ca80
dabeden <dabeden@gmail.com>
2024-11-19 22:12:51 -0800

    Succesfully deployed a landing page for vercel

---
+++ portfolio/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Button} from 'react-bootstrap';
+
+
+function Home() {
+    return (
+    <div className="Home">
+        <h1>Welcome to the home page. Nerd!"</h1>;
+        <Button variant="primary"></Button>
+    </div>
+    );
+}
+
+
+export default Home;
